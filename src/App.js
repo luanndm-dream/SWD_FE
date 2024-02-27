48f21e697325f6cd982ea3028084b395139e6a59
Nguyễn Thành Phú <93639936+thanhphunguyen0310@users.noreply.github.com>
2024-02-27 13:49:53 +0700

    add nav bar from antd

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 // import Login from "./pages/Login/login";
 import "./App.css"
 import Header from './components/Header/Header';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <NavBar/>
       {/* <Login/> */}
     </div>
   );
